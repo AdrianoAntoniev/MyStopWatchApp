7d743f7f7e909e37512ed72cc1a4d216cc709e2d
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2021-01-17 16:20:07 -0800

    Initial Commit

---
+++ MyStopWatchApp/MyStopWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStopWatchAppApp.swift
+//  MyStopWatchApp
+//
+//  Created by Adriano Rodrigues Vieira on 17/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MyStopWatchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
